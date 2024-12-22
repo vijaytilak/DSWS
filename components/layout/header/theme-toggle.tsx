533726db6d65ba33384291bac8b23b471d458724
vijay.tilak <vijaykrishnatilak@gmail.com>
2024-12-22 18:02:00 +1300

    Enhance ThemeToggle component with dynamic styling based on current theme
    - Added theme state to the ThemeToggle component to apply conditional styling to the Button based on the current theme (dark or light).
    - Simplified Button import by removing unnecessary props.

--- components/layout/header/theme-toggle.tsx
+++ components/layout/header/theme-toggle.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button, ButtonProps } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -13,12 +13,18 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="h-8 w-8">
+        <Button 
+          className={`h-8 w-8 ${
+            theme === 'dark' 
+              ? 'bg-slate-950 hover:bg-slate-800 border-slate-700 text-slate-100' 
+              : 'bg-slate-50 hover:bg-slate-200 border-slate-200 text-slate-900'
+          }`}
+        >
           <Sun className="h-[1.1rem] w-[1.1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.1rem] w-[1.1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
